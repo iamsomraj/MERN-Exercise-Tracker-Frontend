58f2f27b8223092924fa5614689d6d67e570b9be
iamsomraj <iamsomraj@gmail.com>
2020-02-25 17:48:20 +0530

    first commit

--- README.md
+++ README.md
@@ -1,3 +1,34 @@
+# MERN-Exercise-Tracker-Backend
+
+MERN-Exercise-Tracker is an application where there are users and for those users, there can be zero or more exercises. The exercises are CRUD supported. The usernames are unique. This project is completely based MERN.
+
+## Developer
+
+LinkedIn : [iamsomraj](https://www.linkedin.com/in/iamsomraj/) 😊
+
+HackerRank: [iamsomraj](https://www.hackerrank.com/iamsomraj?hr_r=1) 😊
+
+Portfolio: [Somraj Mukherjee](https://iamsomraj.github.io/) 😊
+
+Google Play Store: [Somraj Mukherjee](https://play.google.com/store/apps/developer?id=Somraj+Mukherjee) 😊
+
+## Show Your Support
+
+Give me a star ⭐
+
+if this project helped you 👦 👧
+
+## Contributing
+
+Pull requests are welcome. 🤝 For major changes, please open an issue first to discuss what you would like to change. 🙏
+
+Please make sure to update tests as appropriate. ✌
+
+## License
+
+[MIT](https://choosealicense.com/licenses/mit/) 📰
+
+
 This project was bootstrapped with [Create React App](https://github.com/facebook/create-react-app).
 
 ## Available Scripts
--- src/App.js
+++ src/App.js
@@ -1,25 +1,21 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { BrowserRouter as Router, Route } from 'react-router-dom';
+import MenuBar from './Components/MenuBar';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import ExercisesList from './Components/ExercisesList';
+import EditExercise from './Components/EditExercise';
+import CreateExercise from './Components/CreateExercise';
+import CreateUser from './Components/CreateUser';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <MenuBar />
+      <Route path="/" exact component={ExercisesList} />
+      <Route path="/edit/:id" exact component={EditExercise} />
+      <Route path="/create" exact component={CreateExercise} />
+      <Route path="/user" exact component={CreateUser} />
+    </Router>
   );
 }
 
